9f9a1106ffa7976755e3282d01a95cdcad5628e0
Taylor DiPentino <taylor.dipentino@gmail.com>
2019-01-24 17:36:01 -0500

    Increased the bar height.

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int barsize   = 16; /* The size of the bar */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Fira Code:size=10" };
@@ -24,7 +25,6 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-// static const char *tags[] = { "Term", "Web", "Email", "Music", "Free", "Free", "Free", "Free", "Free" };
 static const char *tags[] = { "Term", "Web", "Email", "Music", "Free" };
 
 static const Rule rules[] = {
